dcaf5322e925963e37e1e3be08de5a289fbe3290
Steve Kriz <steve.kriz@gmail.com>
2021-01-26 21:43:55 -0800

    Update for aws.

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 app.get('/', (req, res) => res.redirect('/1'));
 app.use('/:id', express.static(path.join(__dirname, '../public')));
-app.use('/bundle', express.static(path.join(__dirname, '../public/main.js')));
+app.use('/:id/bundle', express.static(path.join(__dirname, '../public/main.js')));
 app.use('/api/reviews', routes);
 
 module.exports = app;
