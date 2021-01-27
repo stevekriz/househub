42448c457699289adccc96e401e7eb7c40095406
Steve Kriz <steve.kriz@gmail.com>
2021-01-26 16:35:37 -0800

    Setup dockerization.

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/fec', {
+mongoose.connect(process.env.CONNECTIONSTRING, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
