8ef7476c4e3b37addc000aab20f14e846f357a16
Steve Kriz <steve.kriz@gmail.com>
2021-01-27 11:11:26 -0800

    Remove fontawesome script and replace with svg.

--- src/components/ModalRatingsHeader.jsx
+++ src/components/ModalRatingsHeader.jsx
@@ -17,14 +17,14 @@ const Container = styled.div`
   }
 `;
 
-const Star = styled.span`
+const Star = styled.svg`
   display: flex;
   align-items: center;
-  color: rgb(255, 56, 92);
-  font-size: 22.76px;
+  fill: rgb(255, 56, 92);
+  width: 32.99px;
   padding-right: 8px;
   @media (max-width: 730px) {
-    font-size: 18.5px;
+    width: 28px;
   }
 `;
 
@@ -44,7 +44,14 @@ const RatingHeader = styled.span`
 
 const ModalRatingsHeader = ({ averageRating, reviewCount }) => (
   <Container>
-    <Star><i className="fas fa-star" /></Star>
+    <Star
+      viewBox="0 0 1000 1000"
+      role="presentation"
+      aria-hidden="true"
+      focusable="false"
+    >
+      <path d="M972 380c9 28 2 50-20 67L725 619l87 280c11 39-18 75-54 75-12 0-23-4-33-12L499 790 273 962a58 58 0 0 1-78-12 50 50 0 0 1-8-51l86-278L46 447c-21-17-28-39-19-67 8-24 29-40 52-40h280l87-279c7-23 28-39 52-39 25 0 47 17 54 41l87 277h280c24 0 45 16 53 40z" />
+    </Star>
     <RatingHeader>{`${averageRating} (${reviewCount} reviews)`}</RatingHeader>
   </Container>
 );
